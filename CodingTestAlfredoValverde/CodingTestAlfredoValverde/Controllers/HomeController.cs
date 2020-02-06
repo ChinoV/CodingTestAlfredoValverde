0bed9b34b1821a0347d2241dd6c7f5974de5e9e7
Alfredo Valverde <alfred.valvg@gmail.com>
2020-02-06 17:25:18 -0600

    Initial Commit, final version.

---
+++ CodingTestAlfredoValverde/CodingTestAlfredoValverde/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CodingTestAlfredoValverde.Models;
+using Newtonsoft.Json;
+
+namespace CodingTestAlfredoValverde.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PersonaViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string filePath = $@"{System.IO.Directory.GetCurrentDirectory()}\Files\CodingTest.txt";
+
+                List<PersonaViewModel> personas = new List<PersonaViewModel>();
+
+
+                if (System.IO.File.Exists(filePath))
+                {
+                    string jsonText = System.IO.File.ReadAllText(filePath);
+                    personas = JsonConvert.DeserializeObject<List<PersonaViewModel>>(jsonText);
+                }
+                personas.Add(model);
+                System.IO.File.WriteAllText(filePath, JsonConvert.SerializeObject(personas));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public string GetJson()
+        {
+            string filePath = $@"{System.IO.Directory.GetCurrentDirectory()}\Files\CodingTest.txt";
+            if (System.IO.File.Exists(filePath))
+            {
+                return System.IO.File.ReadAllText(filePath);
+            }
+            return "";
+        }
+    }
+}
---
+++ CodingTestAlfredoValverde/CodingTestAlfredoValverde/Models/PersonaViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingTestAlfredoValverde.Models
+{
+    public class PersonaViewModel
+    {
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido { get; set; }
+        [Required]
+        [Display(Name = "Teléfono")] 
+        public string Telefono { get; set; }
+        [Required]
+        [Display(Name = "Género")]
+        public string Genero { get; set; }
+        [Display(Name = "Ocupación")]
+        public string Ocupacion { get; set; }
+    }
+}
